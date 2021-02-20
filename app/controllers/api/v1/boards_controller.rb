d0cdab4132c3091528baf6aee21cbff62ada5512
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-02-20 09:29:03 -0500

    build controllers for Boards model and Bulletin model

--- app/controllers/api/v1/boards_controller.rb
+++ app/controllers/api/v1/boards_controller.rb
@@ -1,8 +1,34 @@
 class Api::V1::BoardsController <ApplicationController  
 
     def index 
-      @boards = Board.all 
-      render json: @boards
+      boards = Board.all 
+      render json: boards
+    end 
+
+    def create 
+      board = Board.new(board_params)
+
+      if account.save
+        render json: board 
+      else 
+        render json: {message: 'Board data not found' }
+      end
+    end 
+
+    def show 
+      board = Board.find(params[:id])
+      render json: board
+    end 
+
+    def destroy 
+      board = Board.find(params[:id]) 
+      board.destroy
+    end 
+
+    private 
+
+    def board_params 
+      params.require(:board).permit(:name)
     end
 
 end
--- app/controllers/api/v1/bulletins_controller.rb
+++ app/controllers/api/v1/bulletins_controller.rb
@@ -1,3 +1,35 @@
-class Api::V1::BulletinsController 
+class Api::V1::BulletinsController < ApplicationController
+
+     def index 
+        bulletins = BUlletin.all 
+        render json:  bulletins
+      end 
+  
+      def create 
+        bulletin = Bulletin.new(bulletin_params)
+  
+        if account.save
+          render json: bulletin
+        else 
+          render json: {message: 'Bulletin data not found' }
+        end
+      end 
+  
+      def show 
+        bulletin = Bulletin.find(params[:id])
+        render json: bulletin
+      end 
+  
+      def destroy 
+        bulletin = Board.find(params[:id]) 
+        bulletin.destroy
+      end 
+  
+      private 
+  
+      def bulletin_params 
+        params.require(:bulletin).permit(:title, :content)
+      end
+  
 
 end
