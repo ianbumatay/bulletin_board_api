10e7ed278a4a732c8f9de030b601cf5dbbac8856
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-02-20 00:24:08 -0500

    implement namspace in my controllers

--- app/controllers/bulletins_controller.rb
+++
@@ -1,2 +0,0 @@
-class BulletinsController < ApplicationController
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  resources :bulletins
-  resources :boards
+
+  # resources :boards
+  # resources :bulletins
+
+
+  namespace :api do  
+    namespace :v1 do
+      resources :boards
+      resources :bulletins
+    end 
+  end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
